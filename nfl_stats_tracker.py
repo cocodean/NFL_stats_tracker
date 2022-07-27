631121448696da50f20a6911632ffe4ccebf9583
cocodean <cocodean.a@gmail.com>
2022-07-26 21:48:10 -0700

    added method to retrieve passing stats

--- nfl_stats_tracker.py
+++ nfl_stats_tracker.py
@@ -9,7 +9,13 @@ FOOTBALL_MAIN_URL_PART = r'https://www.pro-football-reference.com'
 FOOTBALL_RUSHING_URL_LAST_PART = r'rushing.htm'
 
 class NFL_Stats:
+    #
+    # Public Constants
+    #
     
+    MAX_PLAYERS_TO_RETURN = 150
+    
+
     '''
     @brief Default constructor
     '''
@@ -18,7 +24,7 @@ class NFL_Stats:
         self.mIsDataLoaded = False                      # Data can be reloaded
         self.mData = None                               # Holds one set of data at a time, for now
         
-#-----------------------------------------------------------------------------#
+    #-------------------------------------------------------------------------#
     
     '''
     @brief Retrieves the rushing statistics for the year selected
@@ -29,11 +35,62 @@ class NFL_Stats:
     def getRushingStats(self, year:int, printToCsv:bool=False):
         print(f'DEBUG: getting rushing stats for the year {year}')
         
-        MAX_PLAYERS_TO_RETURN = 150
-        
         target_url = f'https://www.pro-football-reference.com/years/{year}/rushing.htm'
         
+        stats = self.__getStats__(target_url, 1)
+        
+        # TODO: cleanup player names to remove asterisk (*) and plus-signs (+)
+        
+        stats['Year'] = year
+        
+        if printToCsv:
+            stats.to_csv(f'nfl_rushing_{year}.csv')     # TODO: edit name and path
+        
+        if len(stats) > self.MAX_PLAYERS_TO_RETURN:
+            stats = stats[:self.MAX_PLAYERS_TO_RETURN]
+        
+        return stats
+    
+    #-------------------------------------------------------------------------#
+    
+    def getPassingStats(self, year:int, printToCsv:bool=False):
+        print(f'DEBUG: getting passing stats for the year {year}')
+        
+        target_url = f'https://www.pro-football-reference.com/years/{year}/passing.htm'
+        
+        stats = self.__getStats__(target_url, 0)
+        
+        # TODO: cleanup player names to remove asterisk (*) and plus-signs (+)
+        
+        stats['Year'] = year
+        
+        if printToCsv:
+            stats.to_csv(f'nfl_passing_{year}.csv')     # TODO: edit name and path
+        
+        if len(stats) > self.MAX_PLAYERS_TO_RETURN:
+            stats = stats[:self.MAX_PLAYERS_TO_RETURN]
+        
+        return stats
+    
+    #-------------------------------------------------------------------------#
+    
+#
+# "Private"
+#
+    
+    '''
+    @brief General method to get stats from the site pro football reference.
+    @param[in] target_url A string to represent the site to search
+    @param[in] target_table An int for which table found to use
+    @param[in] num_rows_to_return An int for the number of rows to allow in
+    the return DataFrame
+    @return A pandas DataFrame
+    '''
+    def __getStats__(self, target_url:str, 
+                     target_table:int=0,
+                     num_rows_to_return:int=MAX_PLAYERS_TO_RETURN):
         print(f'DEBUG: fetching info from the url {target_url}')
+        print(f'DEBUG: the targetTable is {target_table}')
         
         try:
             pages = requests.get(target_url)
@@ -45,11 +102,13 @@ class NFL_Stats:
         soup = BeautifulSoup(pages.text, 'lxml')
         
         # HTML tables have the 'tr' tag for rows. So we grab one row and find all its header values; Breaks for multiple tables
-        headers = [th.getText() for th in soup.findAll('tr')[1].findAll('th')]
+        headers = [th.getText() for th in soup.findAll('tr')[target_table].findAll('th')]
         headers = headers[1:] # drop the 'Rk' rank column since it will not match with the table
+        print(f'DEBUG: the headers found for the table are {headers}')
         
         # Get the table rows
         rows = soup.findAll('tr', class_= lambda table_rows: table_rows != 'thead')
+        print(f'DEBUG: the number of data rows found is {len(rows)}')
         
         # Extract player information from the table row tags
         player_stats = [[td.getText() for td in rows[i].findAll('td')] # get the table data cell text from each table data cell
@@ -58,14 +117,9 @@ class NFL_Stats:
         
         stats = pd.DataFrame(player_stats, columns=headers)
         
-        # TODO: cleanup player names to remove asterisk (*) and plus-signs (+)
-        
-        stats['Year'] = year
-        
-        if printToCsv:
-            stats.to_csv(f'nfl_rushing_{year}.csv')     # TODO: edit name and path
-        
-        if len(stats) > MAX_PLAYERS_TO_RETURN:
-            stats = stats[:MAX_PLAYERS_TO_RETURN]
+        if len(stats) > num_rows_to_return:
+            stats = stats[:num_rows_to_return]
         
         return stats
+    
+#-----------------------------------------------------------------------------#
